2083ad4cc6d53e11a5328d4c5b15c194fa939eea
Himesh <himeshpatel0704@gmail.com>
2024-06-23 16:38:04 +0530

    constructer

--- OOPs/Constructors/Task10.java
+++ OOPs/Constructors/Task10.java
@@ -2,23 +2,31 @@
 //Create an immutable 'Point' class with fields 'x' and 'y'. Implement a constructor to initialize these fields and do not provide any setter methods. Ensure that the fields are private and final, and write methods to get the values of 'x' and 'y'.
 
 class  Point{
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
 
     public Point(int x , int y){
         this.x = x;
         this.y = y;
     }
 
-    void getValue(){
-        System.out.println("The value of x is :- "+x);
-        System.out.println("The value of y is :- "+y);
+    public int getX(){
+        return x;
     }
-};
+    public int getY(){
+        return y;
+    }
+    public void getPoints(){
+        System.out.println("("+x+", "+y+")");
+    }
+}
 
 public class Task10 {
     public static void main(String[] args) {
             Point obj = new Point(3, 4);
-            obj.getValue();
+            obj.getPoints();
+
+            Point obj2 = new Point(1, 1);
+            obj2.getPoints();
     }
 }
--- OOPs/Constructors/Task6.java
+++ OOPs/Constructors/Task6.java
@@ -5,12 +5,31 @@ class Time{
     private int hour;
     private int minute;
 
-    public Time(int h , int m){
+    private Time(int h , int m){
         hour = h;
         minute = m;
+    }
+    public static Time of(int hour , int minute){
+        if(hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+            System.out.println("Invalid time. Setting default time to 00:00.");
+            return new Time(00, 00);
+        } else {
+            return new Time(hour, minute);
+        }
+    }
+
+    public void getTimeDetails() {
+        System.out.println(String.format("Time: %02d:%02d", hour, minute));
+        // System.out.printf("Time: %02d:%02d", hour, minute);
     }
 };
 
 public class Task6 {
-    
+    public static void main(String[] args) {
+        Time t1 = Time.of(16, 34);
+        t1.getTimeDetails();
+
+        Time t2 = Time.of(45, 123);
+        t2.getTimeDetails();
+    }
 }
