d8a8ec83e31cbb69a766cbed56fdfc3acbc31102
Himesh <himeshpatel0704@gmail.com>
2024-07-23 22:49:29 +0530

    String

--- String/Task1.java
+++ String/Task1.java
@@ -7,9 +7,29 @@
    - Input: "invalid-email"
      - Expected Output: Invalid email address
  */
+import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the any semple email :- ");
+      String pass = sc.nextLine();
 
+      System.out.println("The String is :- "+pass);
+      if(pass.endsWith(".com")){
+        if (pass.contains("@")){
+          if(pass.contains("mail")){
+            System.out.println("Vailid Email...");
+          }else{
+            System.out.println("Invailid Email...");
+          }
+        }else{
+          System.out.println("Invailid Email...");
+        }
+      }
+      else{
+        System.out.println("Invailid Email...");
+      }
+      sc.close();
     }
 }
